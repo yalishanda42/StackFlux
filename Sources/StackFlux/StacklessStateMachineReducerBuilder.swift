fa331619ca8a7e757fe16a90a690bba67a3ab156
Alexander Ignatov <yalishanda@abv.bg>
2022-07-25 00:19:19 +0300

    Do not use tuples and remove duplicates

--- Sources/StackFlux/StacklessStateMachineReducerBuilder.swift
+++ Sources/StackFlux/StacklessStateMachineReducerBuilder.swift
@@ -2,7 +2,21 @@
 
 @resultBuilder
 public struct StacklessStateMachineReducerBuilder<State: Hashable, Action: Hashable> {
-    public typealias Transition = (State, Action, State)
+    public struct Condition: Hashable {
+        let state: State
+        let action: Action
+    }
+    
+    public struct Transition: Hashable {
+        let condition: Condition
+        let consequence: State
+        
+        init(from: State, taking: Action, to: State) {
+            self.condition = .init(state: from, action: taking)
+            self.consequence = to
+        }
+    }
+    
     public typealias Component = [Transition]
     
     public static func buildExpression(_ element: Transition) -> Component {
@@ -37,19 +51,13 @@ public struct StacklessStateMachineReducerBuilder<State: Hashable, Action: Hasha
         component
     }
     
-    fileprivate struct StateActionPair: Hashable {
-        // that is because implicit tuple conformance to Hashable
-        // is not possible yet
-        let state: State
-        let action: Action
-    }
-    
     public static func buildFinalResult(_ component: Component) -> (State, Action) -> State {
-        // Split transition triplets into
-        // a state-action pair and an associated next state.
-        let transitions = component.map {
-            (StateActionPair(state: $0.0, action: $0.1), $0.2)
-        }
+        let transitions = Set(component) // uniquify
+            .map {
+                // Split transition triplets into
+                // a state-action pair and an associated next state.
+                ($0.condition, $0.consequence)
+            }
         
         // The following will throw if the keys are not unique
         // i.e. if one tries to create a nondeterministic state machine
@@ -60,7 +68,7 @@ public struct StacklessStateMachineReducerBuilder<State: Hashable, Action: Hasha
         // the associated next state with it.
         // If it doesn't find such a transition
         // then no state change should be performed.
-        return { dictionary[StateActionPair(state: $0, action: $1)] ?? $0 }
+        return { dictionary[Condition(state: $0, action: $1)] ?? $0 }
     }
 }
 
@@ -105,16 +113,34 @@ precedencegroup TransitionPrecedence {
     assignment: false
 }
 
-infix operator -->: TransitionPrecedence
-
-public func --><S, A>(lhs: (S, A), rhs: S) -> (S, A, S) {
-    (lhs.0, lhs.1, rhs)
+precedencegroup ConditionPrecedence {
+    higherThan: TransitionPrecedence
+    lowerThan: TernaryPrecedence
+    associativity: none
+    assignment: false
 }
 
-public func --><S: CaseIterable, A>(lhs: A, rhs: S) -> [(S, A, S)] {
-    S.allCases.map { ($0, lhs, rhs) }
+infix operator --> : ConditionPrecedence
+infix operator ==> : TransitionPrecedence
+
+public func --><S, A>(
+    lhs: S,
+    rhs: A
+) -> StacklessStateMachineReducerBuilder<S, A>.Condition {
+    .init(state: lhs, action: rhs)
 }
 
-public func --><S, A: CaseIterable>(lhs: S, rhs: S) -> [(S, A, S)] {
-    A.allCases.map { (lhs, $0, rhs) }
+public func ==><S, A>(
+    lhs: StacklessStateMachineReducerBuilder<S, A>.Condition,
+    rhs: S
+) -> StacklessStateMachineReducerBuilder<S, A>.Transition {
+    .init(from: lhs.state, taking: lhs.action, to: rhs)
+}
+
+public func ==><S: CaseIterable, A>(lhs: A, rhs: S) -> [StacklessStateMachineReducerBuilder<S, A>.Transition] {
+    S.allCases.map { .init(from: $0, taking: lhs, to: rhs) }
+}
+
+public func ==><S, A: CaseIterable>(lhs: S, rhs: S) -> [StacklessStateMachineReducerBuilder<S, A>.Transition] {
+    A.allCases.map { .init(from: lhs, taking: $0, to: rhs) }
 }
--- Tests/StackFluxTests/StacklessStateMachineReducerBuilderTests.swift
+++ Tests/StackFluxTests/StacklessStateMachineReducerBuilderTests.swift
@@ -16,7 +16,7 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
     
     func testOneTransition() {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
-            (.s0, .a1, .s1)
+            .s0 --> .a1 ==> .s1
         }
         
         let result = sut.applied(action: .a1).currentState
@@ -26,7 +26,7 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
     
     func testNonexistingTransitionDoesntChangeState() {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
-            (.s0, .a1, .s1)
+            .s0 --> .a1 ==> .s1
         }
         
         let result = sut.applied(action: .a2).currentState
@@ -36,9 +36,9 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
     
     func testThreeTransitions() {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
-            (.s0, .a1, .s1)
-            (.s1, .a1, .s2)
-            (.s2, .a1, .s0)
+            (.s0 --> .a1) ==> .s1
+            (.s1 --> .a1) ==> .s2
+            (.s2 --> .a1) ==> .s0
         }
         
         let result = sut
@@ -53,7 +53,7 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: [], reducerBuilder: {
             for s in S.allCases {
                 for a in A.allCases {
-                    (s, a, S(rawValue: (s.rawValue + a.rawValue) % S.allCases.count)!)
+                    s --> a ==> S(rawValue: (s.rawValue + a.rawValue) % S.allCases.count)!
                 }
             }
         })
@@ -68,9 +68,9 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
     
     func testArrowSyntaxTransitions() {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
-            (.s0, .a0) --> .s0
-            (.s0, .a1) --> .s1
-            (.s0, .a2) --> .s2
+            (.s0 --> .a0) ==> .s0
+            (.s0 --> .a1) ==> .s1
+            (.s0 --> .a2) ==> .s2
         }
         
         let result = sut
@@ -82,8 +82,22 @@ class StacklessStateMachineReducerBuilderTests: XCTestCase {
     
     func testBatchArrowSyntaxTransitions() {
         let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
-            A.a0 --> .s0
-            A.a1 --> .s1
+            A.a0 ==> .s0
+            A.a1 ==> .s1
+        }
+        
+        let result = sut
+            .applied(action: .a1)
+            .currentState
+        
+        XCTAssertEqual(result, S.s1)
+    }
+    
+    func testMixedArrowSyntaxTransitions() {
+        let sut = StacklessStateMachine<S, A>(initial: .s0, finalStates: []) {
+            A.a0 ==> .s0
+            (.s0 --> .a1) ==> .s1
+            S.s2 ==> .s0
         }
         
         let result = sut
